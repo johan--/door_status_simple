a338a7cad0c70967fef99ae3a119b5ca475f849a
Ryan Boland <bolandryanm@gmail.com>
2014-05-27 21:17:52 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoorStatusSimple::Application.config.secret_key_base = '54fa93c19e50f5fd8d958e354a19ee0918451c7d84180f3f25e09e04a7982f35acf1972e3ed5518e7d4c6fbb1bc7a8983218f6f429126f4a388cf36cfb7b0691'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DoorStatusSimple::Application.config.session_store :cookie_store, key: '_door_status_simple_session'
